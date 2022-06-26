f4f95e24014881e1ace7862420ec805c68e049b2
Nick <mr.upgrade3@protonmail.com>
2022-06-26 15:06:13 +0300

    Save progress

---
+++ script/script.js
@@ -0,0 +1,26 @@
+// *** SELECT ITEMS ***
+
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
+
+
+// Edit options
+
+let editElement;
+let editFlag = false;
+let editID = "";
+
+// *** EVENT LISTENERS ***
+
+
+
+// *** Functions ***
+
+// ** LOCAL STORAGE **
+
+// *** Setup Items ***
