6a84e4feefa84939544ba813bf07e4409eb49af1
Nick <mr.upgrade3@protonmail.com>
2022-06-26 18:18:29 +0300

    Save progress

--- script/script.js
+++ script/script.js
@@ -17,8 +17,9 @@ let editID = "";
 
 // *** EVENT LISTENERS ***
 // submit form
-
 form.addEventListener("submit", addItem);
+// Clear items
+clearBtn.addEventListener("click", clearItems);
 
 
 // *** Functions ***
@@ -27,20 +28,94 @@ function addItem(e) {
     const value = grocery.value;
     const id = new Date().getTime().toString();
     if (value && !editFlag) {
-        console.log("add item to the list");
-    }
-    else if (value && editFlag) {
+        const element = document.createElement("article");
+        // add class
+        element.classList.add("grocery-item");
+        // add id
+        const attr = document.createAttribute("data-id");
+        attr.value = id;
+        element.setAttributeNode(attr);
+        element.innerHTML = `<p class="title">${value}</p>
+            <div class="btn-container">
+              <button type="button" class="edit-btn">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                  <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                </svg>
+              </button>
+              <button type="button" class="delete-btn">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                </svg>
+              </button>
+            </div>`;
+            const deleteBtn = element.querySelector(".delete-btn");
+            const editBtn = element.querySelector(".edit-btn");
+            deleteBtn.addEventListener("click", deleteItem);
+            editBtn.addEventListener("click", editItem);
+            // append child
+            list.appendChild(element);
+            // display alert
+            displayAlert("item added to the list", "success");
+            // show container
+            container.classList.add("show-container");
+            // add to local storage
+            addToLocalStorage(id,value);
+            // Set back to default
+            setBackToDefault();
+    } else if (value && editFlag) {
         console.log("editing");
-    }
-    else {
+    } else {
         displayAlert("please enter value", "danger");
     }
 }
 // Display alert
-function displayAlert(text,action) {
+function displayAlert(text, action) {
     alert.textContent = text;
-    alert.classList.add('alert-${action}');
+    alert.classList.add(`alert-${action}`);
+
+    // Remove alert
+    setTimeout(function () {
+        alert.textContent = "";
+        alert.classList.remove(`alert-${action}`);
+    },1000)
+}
+// Clear items
+
+function clearItems() {
+    const items = document.querySelectorAll(".grocery-item");
+    if (items.length > 0) {
+        items.forEach(function (item) {
+            list.removeChild(item);
+        });
+    }
+    container.classList.remove("show-container");
+    displayAlert("empty list", "danger");
+    setBackToDefault();
+    // localStorage.removeItem("list");
+}
+// Delete function
+
+function deleteItem() {
+    console.log("item deleted");
+}
+
+// Edit item
+
+function editItem() {
+    console.log("item edited");
+}
+
+// Set back to default
+function setBackToDefault() {
+    grocery.value = "";
+    editFlag = false;
+    editID = "";
+    submitBtn.textContent = "submit";
 }
 // ** LOCAL STORAGE **
-
+function addToLocalStorage(id,value) {
+    console.log("added to local storage");
+}
 // *** Setup Items ***
